09e71a7f8dd0407af2aa88925c1fc2b41d3a25e4
JacopoOrlandini <jorla94@gmail.com>
2018-10-26 09:12:04 +0200

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -4,36 +4,50 @@
 
 // Helper functions and utilities to work with CUDA
 #include "device_launch_parameters.h"
-#include "cuda.h"
+
+//Standard C library
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-#define subCOL 16
+#define subCOL 5248
 #define COL 5248
 #define ROW 358
-#define subMatDim subCOL*ROW
+#define WARPABLEROW 512 
+#define blocksize 256
+#define subMatDim subCOL*WARPABLEROW
 #define targetMatDim ROW * COL
-
-__global__ void kernel(float *g_idata, float *g_odata);
-
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
+{
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
+}
+template <unsigned int blockSize> __global__ void reduce(float *g_idata, float *g_odata);
+template <unsigned int blockSize> __device__ void warpReduce(volatile float* sdata, int tid);
 float convesionFloat(char *str);
 char* mystrsep(char **stringp, const char *delim);
 void stampa_mat(float*a);
 
 
 int main(int argc, char *argv[])	{
+	printf("Running app: %s\nINFO\n\n", argv[0]);
+	printf("--------------------------\n");
+	printf("COL = %d\nROW = %d\nsubCOL = %d\nWarpable row = %d\n blocksize = %d\n", COL, ROW, subCOL,WARPABLEROW,blocksize);
+	printf("Executing on s00_Target.txt\n");
+	printf("--------------------------\n\n");
+
 	/*Declaration*/
-	fprintf(stdin, "Inizio main\n");
 	float *dev_input, *dev_output;
-	float *subMatrix, *host_output;
+	float *subMatrix;
 	float **A = new float*[ROW];
+	float *h_b;		
 
-		
-	printf("Main initialitation\n");
-
-	FILE *file;
+	FILE *file,*output;
 
 	/*Memory allocation*/
 	printf("GPU and CPU: Allocating memory\n");
@@ -47,30 +61,34 @@ int main(int argc, char *argv[])	{
 	printf("Set device 0\n");
 
 	// Allocate 2 GPU buffers and 1 CPU buffer
-	cudaStatus = cudaMallocHost((void**)&subMatrix, subMatDim * sizeof(float));
+	/*Versione pinned*/
+	cudaStatus = cudaMallocHost(&subMatrix, subMatDim * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMallocHost failed!");
-		//goto Error;
-	}
+		goto Error;
+	}/**/
+	/*Version unpinned
+	subMatrix = (float*)malloc(sizeof(float)*subMatDim);
+	/**/
 	printf("Cuda malloc host on subMatrix\n");
 	
-	cudaStatus = cudaMallocHost((void**)&host_output, subCOL * sizeof(float));
+	cudaStatus = cudaMallocHost(&h_b, subCOL * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMallocHost failed!");
-	//	goto Error;
-	}		
-	printf("Cuda malloc host on host_result\n");
+		fprintf(stderr, "cudaMallocHost failed h_b!");
+		goto Error;
+	}
+	printf("Cuda malloc host on h_b\n");
 
-	cudaStatus = cudaMalloc((void**)&dev_input, subMatDim * sizeof(float));
+	cudaStatus = cudaMalloc(&dev_input, subMatDim * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
-		//goto Error;
+		goto Error;
 	}
 	printf("CudaMalloc on dev_input\n");
-	cudaStatus = cudaMalloc((void **)&dev_output, subCOL * sizeof(float));
+	cudaStatus = cudaMalloc(&dev_output, subCOL * sizeof(float));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc failed!");
-		//goto Error;
+		goto Error;
 	}
 	printf("CudaMalloc on dev_output\n");
 	
@@ -81,65 +99,97 @@ int main(int argc, char *argv[])	{
 	if (file == NULL)
 	{
 		printf("Error opening file\n");
-		//goto Error;
+		goto Error;
 	}
 	printf("Initialitation of Matrix A\n");
 
-	//Instantiating matrix A
-
+	//Instantiating matrix : target 358*5248
 	A[0] = new float[ROW * COL];
 	for (int i = 1; i < ROW; ++i)
 		A[i] = A[i - 1] + COL;
 	for (int i = 0; i < ROW; i++) {
 		for (int j = 0; j < COL; j++) {
-			char *string = (char *)malloc(sizeof(char) * 100);
+			char *string = (char *)malloc(sizeof(char) * 40);
 			if (fscanf(file, "%s ", string) <= 0)
 				perror("Error reading from file\n");
 			A[i][j] = convesionFloat(string);
 		}
 	}
 	//Instatiating subMatrix
-	printf("Initialitatio of subMatrix\n");
-	for (int i = 0; i < subCOL; ++i) {
-		for (int j = 0; j < ROW; ++j) {
-			subMatrix[i * ROW + j ] = A[j][i];
-			}
+	for (int i = 0; i < subCOL; i++) {
+		for (int j = 0; j < WARPABLEROW; j++) {
+			if(j > 357)//359-esimo elemento
+				subMatrix[j + i * WARPABLEROW] = 0;
+			else
+			subMatrix[j + i* WARPABLEROW] = A[j][i];
+		}
 	}
-	printf("\n");
-	
-	cudaStatus = cudaMemcpy(dev_input, subMatrix, sizeof(subMatrix), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "CudaMemCpy failed input!");
-	}
-	printf("Copied subMatrix memory to dev_a\n");
-	dim3 blocksize(ROW, 1, 1);//numero di thread corrispondenti alle righe
+	printf("Instantiation subMatrix");
+	gpuErrchk(cudaMemcpy(dev_input, subMatrix, subMatDim * sizeof(float) , cudaMemcpyHostToDevice));
+	printf("Copied subMatrix memory to dev_input\n");
+	dim3 blockSize(blocksize,1,1);//numero di thread corrispondenti alle righe
 	printf("Created blocksize\n");
-	dim3 gridsize(subCOL, 1, 1); //numero di blocchi corrispondenti alle colonne
+	dim3 gridsize(subCOL,1,1); //numero di blocchi corrispondenti alle colonne
 	printf("Created gridSize\n");
-	kernel <<<gridsize, blocksize>>> (dev_input, dev_output);
-	printf("Kernel completed\n");
-	cudaStatus = cudaMemcpy(host_output, dev_output, sizeof(host_output), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "CudaMemCpy failed output!");
-	}
+	reduce<blocksize><<<gridsize, blockSize >>> (dev_input, dev_output);
+	cudaDeviceSynchronize();
+	printf("\nKernel completed\n");
+	cudaMemcpy(h_b, dev_output, sizeof(float)*subCOL , cudaMemcpyDeviceToHost);
 	printf("Transfer dev_output to b\n");
-	for (int i = 0; i < 1; i++)	printf("mean value = %f\n", host_output[0]);
+	output = fopen("target.csv","w");
+	for (int i = 0; i < subCOL; i++)	
+		fprintf(output,"%f,",h_b[i]);
+	printf("[SUCCESS]Written file output");
+	
+	fclose(output);
+	fclose(file);
+	cudaFreeHost(subMatrix);
+	cudaFreeHost(h_b);
+	cudaFree(dev_input);
+	cudaFree(dev_output);
 
 	cudaStatus = cudaDeviceReset();
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceReset failed!");
+		fprintf(stderr, "cudaDeviceReset failed!\n");
 		return 1;
 	}
 	printf("All right, close program\n");
 	return 1;
 Error:
-	cudaFree(subMatrix);
-	cudaFree(dev_input);
-	cudaFree(dev_output);
-	printf("Deallocated memory\n");
+	printf("Error on program, deallocating memory\n");
 	return 0;
 }
 
+
+
+template <unsigned int blockSize> __global__ void reduce(float *g_idata, float *g_odata) {
+	__shared__ float sdata[blockSize];
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockSize *2 + threadIdx.x;
+	sdata[tid] = g_idata[i] + g_idata[i + blockSize];
+	__syncthreads();
+	//if (blockSize >= 512) { if (tid < 256) { sdata[tid] += sdata[tid + 256]; } __syncthreads(); }
+	if (blockSize >= 256) { if (tid < 128) { sdata[tid] += sdata[tid + 128]; } __syncthreads(); }
+	if (blockSize >= 128) { if (tid < 64) { sdata[tid] += sdata[tid + 64]; } __syncthreads(); }
+	if (tid < 32) {
+		if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+		if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+		if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+		if (blockSize >= 8) sdata[tid] += sdata[tid + 4];
+		if (blockSize >= 4) sdata[tid] += sdata[tid + 2];
+		if (blockSize >= 2) sdata[tid] += sdata[tid + 1];
+	}
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0]/ROW;
+
+}
+template <unsigned int blockSize> __device__ void warpReduce(volatile float* sdata, int tid) {
+	if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+	if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+	if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+	if (blockSize >= 8) sdata[tid] += sdata[tid + 4];
+	if (blockSize >= 4) sdata[tid] += sdata[tid + 2];
+	if (blockSize >= 2) sdata[tid] += sdata[tid + 1];
+}
 
 float convesionFloat(char *str) {
 	float result = -99;
@@ -165,37 +215,135 @@ char* mystrsep(char **stringp, const char *delim)
 }
 
 void stampa_mat(float*a) {
-	int i, j;
+	int i;
 
 	for (i = 0; i < ROW; ++i) {
-		for (j = 0; j < subCOL; ++j) {
-			if( i == 0)
-				printf("%f, ", a[j + i * ROW]);
-		}
-		printf("\n");
+		printf("%f\t ", a[i]);
+		if (i % 10 == 0 && i > 0)
+			printf("\n");
 	}
 }
 
-__global__ void kernel(float *g_idata, float *g_odata) {
+/*Unused Version of reduce
+*g_size not used for this project: erasable
+*/
 
-	__shared__ float sdata[ROW];
-
-	int i = blockIdx.x *gridDim.x*threadIdx.x;
-	int tid = threadIdx.x;
+__global__ void reduce1(int *g_idata, int *g_odata, int g_size)
+{
+	__shared__ int sdata[blocksize];
+	// each thread loads one element from global to shared mem
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
 	sdata[tid] = g_idata[i];
 	__syncthreads();
-	for (int s = 1; s < blockDim.x; s *= 2)
+	// do reduction in shared mem
+	for (unsigned int s = 1; s < blockDim.x; s *= 2)
 	{
-		int index = 2 * s * threadIdx.x;
-		if (index < blockDim.x) {
-			if ((index + s) >= blockDim.x)
-				sdata[index + s] = 0;
-			sdata[index] += sdata[index + s];
+		if (tid % (2 * s) == 0) {
+			sdata[tid] += sdata[tid + s];
 		}
 		__syncthreads();
 	}
-	if (tid == 0) {
-		g_odata[blockIdx.x] = sdata[0]/ROW;
+	// write result for this block to global mem
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+__global__ void reduce2(int *g_idata, int *g_odata, int g_size)
+{
+	__shared__ int sdata[blocksize];
+	// each thread loads one element from global to shared mem
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+	sdata[tid] = g_idata[i];
+	__syncthreads();
+	// do reduction in shared mem
+	for (unsigned int s = 1; s < blockDim.x; s *= 2)
+	{
+		int index = 2 * s*tid;
+
+		if (index < blockDim.x)
+		{
+			sdata[index] += sdata[index + s];
+		}
+
 		__syncthreads();
 	}
+	// write result for this block to global mem
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+__global__ void reduce3(int *g_idata, int *g_odata, int g_size)
+{
+	__shared__ int sdata[blocksize];
+	// each thread loads one element from global to shared mem
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+	sdata[tid] = g_idata[i];
+	__syncthreads();
+	// do reduction in shared mem
+	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+	{
+		if (tid < s)
+		{
+			sdata[tid] += sdata[tid + s];
+		}
+
+		__syncthreads();
+	}
+	// write result for this block to global mem
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+__global__ void reduce4(int *g_idata, int *g_odata, int g_size)
+{
+	__shared__ int sdata[blocksize];
+	// each thread loads one element from global to shared mem
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*(blockDim.x * 2) + threadIdx.x;
+	sdata[tid] = g_idata[i] + g_idata[i + blockDim.x];
+	__syncthreads();
+	// do reduction in shared mem
+	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+	{
+		if (tid < s)
+		{
+			sdata[tid] += sdata[tid + s];
+		}
+
+		__syncthreads();
+	}
+	// write result for this block to global mem
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+
+__global__ void reduce5(int *g_idata, int *g_odata, int g_size)
+{
+	__shared__ int sdata[blocksize];
+	// each thread loads one element from global to shared mem
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x*(blockDim.x * 2) + threadIdx.x;
+	sdata[tid] = g_idata[i] + g_idata[i + blockDim.x];
+	__syncthreads();
+	// do reduction in shared mem
+	for (unsigned int s = blockDim.x / 2; s > 32; s >>= 1)
+	{
+		if (tid < s)
+		{
+			sdata[tid] += sdata[tid + s];
+		}
+
+		__syncthreads();
+	}
+	if (tid < 32)
+	{
+		sdata[tid] += sdata[tid + 32];
+		sdata[tid] += sdata[tid + 16];
+		sdata[tid] += sdata[tid + 8];
+		sdata[tid] += sdata[tid + 4];
+		sdata[tid] += sdata[tid + 2];
+		sdata[tid] += sdata[tid + 1];
+	}
+	// write result for this block to global mem
+	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
